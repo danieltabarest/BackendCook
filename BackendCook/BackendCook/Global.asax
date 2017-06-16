9bbc37534f6d8b39b181a831f54c6f9978029c1b
danieltabares <danitabares5@hotmail.com>
2017-06-15 19:56:01 -0500

    first version

---
+++ BackendCook/BackendCook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BackendCook.MvcApplication" Language="C#" %>
---
+++ BackendCook/Domain/Ingrediente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Ingrediente
+    {
+        public int IngredienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion{ get; set; }
+
+    }
+}
